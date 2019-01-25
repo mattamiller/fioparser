78e208424aee83fd8e90af0294f94650db377eb3
Matt Miller <mattmiller0526@gmail.com>
2019-01-25 09:55:22 -0600

    Fixed typos in var names

--- fioparser.py
+++ fioparser.py
@@ -14,15 +14,15 @@ for filename in os.listdir(fio_results_dir):
         rjson = f.read()
         rdata = json.loads(rjson)
 
-        ninetieth_percentile = str((rdata['jobs'][0]['read']['clat_ns']['percentile']['95.000000'])*.000001)
-        ninetieninth_percentile = str((rdata['jobs'][0]['read']['clat_ns']['percentile']['99.000000'])*.000001)
+        ninetyfifth_percentile = str((rdata['jobs'][0]['read']['clat_ns']['percentile']['95.000000'])*.000001)
+        ninetyninth_percentile = str((rdata['jobs'][0]['read']['clat_ns']['percentile']['99.000000'])*.000001)
         bw_mean = str((rdata['jobs'][0]['read']['bw_mean'])*.0001)
 
         # Write variables to csv in the following format:
         output.write("" + device_name+","
                      + filename+","
-                     + ninetieth_percentile+","
-                     + ninetieninth_percentile+","
+                     + ninetyfifth_percentile+","
+                     + ninetyninth_percentile+","
                      + bw_mean+"\n"
                      )
 
