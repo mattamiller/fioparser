c957675a6e69b6663e3caf95a7325cc0490142e2
Matt Miller <mattmiller0526@gmail.com>
2019-01-25 09:49:42 -0600

    cleaned up and ran code
    Cleaned up the output.write section and ran the script to get a sample output file.

--- fioparser.py
+++ fioparser.py
@@ -5,7 +5,7 @@ import json
 # set variables manually
 fio_results_dir = '/Users/matthew.miller/Desktop/fio/intel_ssd/'
 device_name = "SampleIntelSSD"
-output = open(device_name+".txt", "w+")
+output = open(device_name+"_output.txt", "w+")
 
 # Loop through JSON and pull out meaningful metrics
 for filename in os.listdir(fio_results_dir):
@@ -19,10 +19,11 @@ for filename in os.listdir(fio_results_dir):
         bw_mean = str((rdata['jobs'][0]['read']['bw_mean'])*.0001)
 
         # Write variables to csv in the following format:
-        output.write("" + device_name+"," )
-        output.write("" + filename+",")
-        output.write("" + ninetieth_percentile +",")
-        output.write("" + ninetieninth_percentile+",")
-        output.write("" + bw_mean+"\n")
+        output.write("" + device_name+","
+                     + filename+","
+                     + ninetieth_percentile+","
+                     + ninetieninth_percentile+","
+                     + bw_mean+"\n"
+                     )
 
 output.close()
