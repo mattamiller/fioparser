0276e3594ced9ad8eb01bc5f9e944fb385938d80
Matt Miller <mattmiller0526@gmail.com>
2019-01-25 09:31:03 -0600

    Comments and +README
    Added comments to fioparser.py and created the readme.md

---
+++ README.md
@@ -0,0 +1,9 @@
+Python parser for FIO results
+
+Set 'fio_results_variable' to the local directory with the FIO results.  This should be set at the directory level named after the storage device tested.  For example, if testing an Intel SSD, your folder level would be something like this "~/intel_ssd/"
+
+Set 'output' to local file for results csv
+
+Set 'device_name' to an identifiable name of the storage device tested
+
+Every time you run this the output files will overwrite the file if the name is the same
--- fioparser.py
+++ fioparser.py
@@ -2,11 +2,12 @@
 import os
 import json
 
+# set variables manually
 fio_results_dir = '/Users/matthew.miller/Desktop/fio/intel_ssd/'
-scrapper_output = 'output/'
-output = open("summary_intelssd.txt", "w+")
-device_name = "Sample Intel SSD"
+device_name = "SampleIntelSSD"
+output = open(device_name+".txt", "w+")
 
+# Loop through JSON and pull out meaningful metrics
 for filename in os.listdir(fio_results_dir):
     if filename.endswith("fio.json"):
         f = open(fio_results_dir+filename)
@@ -18,6 +19,7 @@ for filename in os.listdir(fio_results_dir):
         ninetieninth_percentile = str((rdata['jobs'][0]['read']['clat_ns']['percentile']['99.000000'])*.000001)
         bw_mean = str((rdata['jobs'][0]['read']['bw_mean'])*.0001)
 
+        # Write variables to csv in the following format:
         output.write("" + device_name+"," )
         output.write("" + test_name+",")
         output.write("" + ninetieth_percentile +",")
