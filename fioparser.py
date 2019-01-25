e5db7dc9cca90b7a66c5a22b24220ec1bf732dcd
Matt Miller <mattmiller0526@gmail.com>
2019-01-25 09:36:43 -0600

    Removed unnecessary var
    test_name var was not actually needed

--- fioparser.py
+++ fioparser.py
@@ -14,14 +14,13 @@ for filename in os.listdir(fio_results_dir):
         rjson = f.read()
         rdata = json.loads(rjson)
 
-        test_name = filename
         ninetieth_percentile = str((rdata['jobs'][0]['read']['clat_ns']['percentile']['95.000000'])*.000001)
         ninetieninth_percentile = str((rdata['jobs'][0]['read']['clat_ns']['percentile']['99.000000'])*.000001)
         bw_mean = str((rdata['jobs'][0]['read']['bw_mean'])*.0001)
 
         # Write variables to csv in the following format:
         output.write("" + device_name+"," )
-        output.write("" + test_name+",")
+        output.write("" + filename+",")
         output.write("" + ninetieth_percentile +",")
         output.write("" + ninetieninth_percentile+",")
         output.write("" + bw_mean+"\n")
